0af06278e4123084cbe317253974cfb211edd150
Bridget Leonard <leonardbridget01@gmail.com>
2024-03-05 17:12:05 -0800

    adjust coefficient saving

--- container/language_voxelwise_cm.py
+++ container/language_voxelwise_cm.py
@@ -53,14 +53,17 @@ undertheinfluence_fmri, undertheinfluence_features = prep_data(fmri_undertheinfl
 
 fmri_arrays = [ai_fmri, avatar_fmri, howtodraw_fmri, legacy_fmri,
                life_fmri, yankees_fmri, naked_fmri,
-               odetostepfather_fmri, souls_fmri, undertheinfluence_fmri]
+               odetostepfather_fmri, souls_fmri]
 feature_arrays = [ai_features, avatar_features, howtodraw_features,
                   legacy_features, life_features, yankees_features,
                   naked_features, odetostepfather_features,
-                  souls_features, undertheinfluence_features]
+                  souls_features]
 # Combine data
 Y_train = np.vstack(fmri_arrays)
 X_train = np.vstack(feature_arrays)
+
+Y_test = undertheinfluence_fmri
+X_test = undertheinfluence_features
 
 # Define cross-validation
 run_onsets = []
@@ -102,5 +105,30 @@ pipeline
 
 _ = pipeline.fit(X_train, Y_train)
 
-np.save('results/story/' + subject + 'best_alphas.npy', pipeline.alpha_)
-np.save('results/story/' + subject + 'coefficients.npy', pipeline.coef_)
+# Calculate scores
+scores = pipeline.score(X_test, Y_test)
+print("(n_voxels,) =", scores.shape)
+scores = backend.to_numpy(scores)
+
+best_alphas = backend.to_numpy(pipeline[-1].best_alphas_)
+primal_coef = pipeline[-1].get_primal_coef()
+primal_coef = backend.to_numpy(primal_coef)
+print("(n_delays * n_features, n_voxels) =", primal_coef.shape)
+
+# Regularize coefficients
+primal_coef /= np.linalg.norm(primal_coef, axis=0)[None]
+primal_coef *= np.sqrt(np.maximum(0, scores))[None]
+
+# split the ridge coefficients per delays
+delayer = pipeline.named_steps['delayer']
+primal_coef_per_delay = delayer.reshape_by_delays(primal_coef, axis=0)
+print("(n_delays, n_features, n_voxels) =", primal_coef_per_delay.shape)
+del primal_coef
+
+# average over delays
+average_coef = np.mean(primal_coef_per_delay, axis=0)
+print("(n_features, n_voxels) =", average_coef.shape)
+del primal_coef_per_delay
+
+np.save('results/story/' + subject + 'best_alphas.npy', best_alphas)
+np.save('results/story/' + subject + 'coefficients.npy', average_coef)
