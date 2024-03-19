032dd6be16c6ddcc5c79fbef59919eb53ba884e4
Bridget Leonard <leonardbridget01@gmail.com>
2024-03-19 15:30:21 -0700

    work on protocol

--- container/crossmodal.py
+++ container/crossmodal.py
@@ -10,6 +10,7 @@ from sklearn.model_selection import check_cv
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
 from sklearn import set_config
+from functions import remove_nan
 from functions import prep_data
 from functions import generate_leave_one_run_out
 from functions import Delayer
@@ -317,7 +318,7 @@ def alignment(layer):
     return coef_images_to_captions, coef_captions_to_images
 
 
-def movie_to_story(subject, layer):
+def vision_model(subject, layer):
 
     # Extract features from raw stimuli
     train00 = get_movie_features('data/raw_stimuli/shortclips/stimuli/train_00.hdf', layer)
@@ -335,11 +336,96 @@ def movie_to_story(subject, layer):
     test = get_movie_features('data/raw_stimuli/shortclips/stimuli/test.hdf')
 
     # Project into cross modal space
-    ## Build alignment matrix
+    # Build alignment matrix
     coef_images_to_captions, coef_captions_to_images = alignment(layer)
 
     # Build encoding model
-    
+    print("Load movie data")
+    # Load fMRI data
+    # Using all data for cross-modality encoding model
+    fmri_train = np.load("data/moviedata/" + subject + "/train.npy")
+    fmri_test = np.load("data/moviedata/" + subject + "/test.npy")
+
+    # Prep data
+    train_fmri = remove_nan(fmri_train)
+    test_fmri = remove_nan(fmri_test)
+
+    fmri_arrays = [train_fmri, test_fmri]
+    feature_arrays = [train00, train01, train02, train03, train04,
+                      train05, train06, train07, train08, train09,
+                      train10, train11, test]
+
+    # Combine data
+    Y_train = np.vstack(fmri_arrays)
+    X_train = np.vstack(feature_arrays)
+
+    # Define cross-validation
+    run_onsets = []
+    current_index = 0
+    for arr in feature_arrays:
+        next_index = current_index + arr.shape[0]
+        run_onsets.append(current_index)
+        current_index = next_index
+
+    n_samples_train = X_train.shape[0]
+    cv = generate_leave_one_run_out(n_samples_train, run_onsets)
+    cv = check_cv(cv)  # copy the cross-validation splitter into a reusable list
+
+    # Define the model
+    scaler = StandardScaler(with_mean=True, with_std=False)
+
+    delayer = Delayer(delays=[1, 2, 3, 4])
+
+    backend = set_backend("torch_cuda", on_error="warn")
+    print(backend)
+
+    X_train = X_train.astype("float32")
+
+    alphas = np.logspace(1, 20, 20)
+
+    ridge_cv = RidgeCV(
+        alphas=alphas, cv=cv,
+        solver_params=dict(n_targets_batch=500, n_alphas_batch=5,
+                           n_targets_batch_refit=100))
+
+    pipeline = make_pipeline(
+        scaler,
+        delayer,
+        ridge_cv,
+    )
+
+    set_config(display='diagram')  # requires scikit-learn 0.23
+    pipeline
+
+    _ = pipeline.fit(X_train, Y_train)
+
+    # # Calculate scores
+    # scores = pipeline.score(X_test, Y_test)
+    # print("(n_voxels,) =", scores.shape)
+    # scores = backend.to_numpy(scores)
+
+    # best_alphas = backend.to_numpy(pipeline[-1].best_alphas_)
+    coef = pipeline[-1].coef_
+    coef = backend.to_numpy(coef)
+    print("(n_delays * n_features, n_voxels) =", coef.shape)
+
+    # Regularize coefficients
+    coef /= np.linalg.norm(coef, axis=0)[None]
+    # coef *= np.sqrt(np.maximum(0, scores))[None]
+
+    # split the ridge coefficients per delays
+    delayer = pipeline.named_steps['delayer']
+    coef_per_delay = delayer.reshape_by_delays(coef, axis=0)
+    print("(n_delays, n_features, n_voxels) =", coef_per_delay.shape)
+    del coef
+
+    # average over delays
+    average_coef = np.mean(coef_per_delay, axis=0)
+    print("(n_features, n_voxels) =", average_coef.shape)
+    del coef_per_delay
+
+    return average_coef
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 4: 
@@ -347,42 +433,7 @@ if __name__ == "__main__":
         modality = sys.argv[2]
         layer = sys.argv[3]
 
-        # Define Model
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        model = BridgeTowerModel.from_pretrained("BridgeTower/bridgetower-base")
-        model = model.to(device)
 
-        # Define layers
-        model_layers = {
-                1: model.cross_modal_text_transform,
-                2: model.cross_modal_image_transform,
-                3: model.token_type_embeddings,
-                4: model.vision_model.visual.ln_post,
-                5: model.text_model.encoder.layer[-1].output.LayerNorm,
-                6: model.cross_modal_image_layers[-1].output,
-                7: model.cross_modal_text_layers[-1].output,
-                8: model.cross_modal_image_pooler,
-                9: model.cross_modal_text_pooler,
-                10: model.cross_modal_text_layernorm,
-                11: model.cross_modal_image_layernorm,
-                12: model.cross_modal_text_link_tower[-1],
-                13: model.cross_modal_image_link_tower[-1],
-            }
-
-        # Extract features from raw stimuli
-        train00 = get_movie_features('data/raw_stimuli/shortclips/stimuli/train_00.hdf')
-        train00 = get_movie_features('data/raw_stimuli/shortclips/stimuli/train_00.hdf')
-        train00 = get_movie_features('data/raw_stimuli/shortclips/stimuli/train_00.hdf')
-        train00 = get_movie_features('data/raw_stimuli/shortclips/stimuli/train_00.hdf')
-        train00 = get_movie_features('data/raw_stimuli/shortclips/stimuli/train_00.hdf')
-        train00 = get_movie_features('data/raw_stimuli/shortclips/stimuli/train_00.hdf')
-        train00 = get_movie_features('data/raw_stimuli/shortclips/stimuli/train_00.hdf')
-        train00 = get_movie_features('data/raw_stimuli/shortclips/stimuli/train_00.hdf')
-        train00 = get_movie_features('data/raw_stimuli/shortclips/stimuli/train_00.hdf')
-
-        # Project into cross modal space
-
-        # Build encoding model and predict
     else:
         print("This script requires exactly two arguments: subject, modality, and layer. \
               Ex. python crossmodal.py S1 vision 1")
